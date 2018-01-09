6b97c1c227be8751ea6a309f16b231c0fa9d9459
cabrerap <mail.pablocabrera@gmail.com>
2018-01-09 17:20:25 +0100

    modifications sur le script de déploiement pour faciliter la compréhension

--- deploy_openstack.sh
+++ deploy_openstack.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Launch this script to deploy on both instances, or connect via ssh and run commands line by line to locate an error
+#To deploy faster, use this script on each instance seperately
+
 network()
 {
     #Configure the network
@@ -22,11 +25,91 @@ openstack_install()
     yum install -y centos-release-openstack-pike
     yum update -y
     #Install the PackStack installer
-    yum install -y python-openstackclient
-    yum install -y openstack-selinux
+    #yum install -y python-openstackclient
+    #yum install -y openstack-selinux
+    sudo yum install -y openstack-packstack
 }
 
-database_install()
+openstack_controller()
+{
+    #Create the gen-answer-file
+    packstack --gen-answer-file=ensimag-packstack.txt
+    sed -i -e 's/CONFIG_NTP_SERVERS=.*/CONFIG_NTP_SERVERS=10.3.252.26/g' ensimag-packstack.txt
+    sed -i -e 's/CONFIG_NEUTRON_ML2_TYPE_DRIVERS=.*/CONFIG_NEUTRON_ML2_TYPE_DRIVERS=vxlan,flat,vlan/g' ensimag-packstack.txt
+    sed -i -e 's/CONFIG_NEUTRON_ML2_FLAT_NETWORKS=.*/CONFIG_NEUTRON_ML2_FLAT_NETWORKS=extnet/g' ensimag-packstack.txt
+    sed -i -e 's/CONFIG_NEUTRON_ML2_VLAN_RANGES=.*/CONFIG_NEUTRON_ML2_VLAN_RANGES=extnet:2232:2232/g' ensimag-packstack.txt
+    sed -i -e 's/CONFIG_NEUTRON_OVS_BRIDGE_IFACES=.*/CONFIG_NEUTRON_OVS_BRIDGE_IFACES=br-ex:eno1/g' ensimag-packstack.txt
+    sed -i -e 's/CONFIG_NEUTRON_OVS_BRIDGEs_COMPUTE=.*/CONFIG_NEUTRON_OVS_BRIDGEs_COMPUTE=br-ex/g' ensimag-packstack.txt
+    sed -i -e 's/CONFIG_PROVISION_DEMO=.*/CONFIG_PROVISION_DEMO=n/g' ensimag-packstack.txt
+
+    #Deploy PackStack
+    sudo packstack --answer-file=ensimag-packstack.txt
+
+    #At this point, try accessing OpenStack via Horizon
+    #Ex : 'http://10.11.51.144/dashboard'
+
+    #Source the rc file
+    source keystonerc_admin
+
+    ##Configure OpenStack
+    #openstack user create --domain default --password-prompt neutron
+    #openstack role add --project services --user neutron admin
+    #openstack service create --name neutron --description "OpenStack Networking" network
+    ##Endpoints
+    #openstack endpoint create --region RegionOne network public http://controller:9696
+    #openstack endpoint create --region RegionOne network internal http://controller:9696
+    #openstack endpoint create --region RegionOne network admin http://controller:9696
+
+    #Connect with the external network
+    neutron net-create --router:external --provider:network_type vlan --provider:physical_network extnet --provider:segmentation_id 2232 public
+    neutron subnet-create --name public-subnet --enable_dhcp=False --allocation-pool=start=10.11.54.70,end=10.11.54.89 --gateway=10.11.54.1 public 10.11.54.1/24
+}
+
+
+# Use this to start the vpn in a separate console
+# 'sudo openvpn --config vpnlab2017.conf'
+
+
+echo "Connecting to distant host @10.11.51.144"
+ssh -o StrictHostKeyChecking=no root@10.11.51.144 "$(typeset -f); network; openstack_install; openstack_controller"
+
+
+echo "Connecting to distant host @10.11.51.145"
+ssh -o StrictHostKeyChecking=no root@10.11.51.145 "$(typeset -f); network; openstack_install; openstack_controller"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#OpenStack
+
+database_install() #Used for OpenStack
 {
     yum install -y mariadb mariadb-server python2-PyMySQL
     touch /etc/my.cnf.d/openstack.cnf
@@ -42,7 +125,7 @@ database_install()
     mysql_secure_installation
 }
 
-message_queue() #Not used
+message_queue() #Used for OpenStack
 {
     mysql_secure_installation
     systemctl enable rabbitmq-server.service
@@ -51,7 +134,15 @@ message_queue() #Not used
     rabbitmqctl set_permissions openstack ".*" ".*" ".*"
 }
 
-services_install()
+memcache() #Used for OpenStack
+{
+    yum install memcached python-memcached
+    sed -i -e 's/OPTIONS="-l 127.0.0.1,::1"/OPTIONS="-l 127.0.0.1,::1,controller"/g' /etc/sysconfig/memcached
+    systemctl enable memcached.service
+    systemctl start memcached.service
+}
+
+services_install() #Used for OpenStack
 {
     #Prepare database
     mysql -u root -p
@@ -75,50 +166,19 @@ services_install()
       --bootstrap-region-id RegionOne
 
     #Configure the Apache HTTP server
-    #https://docs.openstack.org/keystone/pike/install/keystone-install-rdo.html
+    sed -i -e 's/ServerName.*/ServerName\ controller/g' /etc/httpd/conf/httpd.conf
+    ln -s /usr/share/keystone/wsgi-keystone.conf /etc/httpd/conf.d/
+
+    #Finalize installation
+    systemctl enable httpd.service
+    systemctl start httpd.service
+    #export OS_USERNAME=admin
+    #export OS_PASSWORD=mario4
+    #export OS_PROJECT_NAME=admin
+    #export OS_USER_DOMAIN_NAME=Default
+    #export OS_PROJECT_DOMAIN_NAME=Default
+    #export OS_AUTH_URL=http://controller:35357/v3
+    #export OS_IDENTITY_API_VERSION=3
+
     #https://docs.openstack.org/install-guide/openstack-services.html
 }
-
-openstack_controller()
-{
-    #Create the gen-answer-file
-    packstack --gen-answer-file=ensimag-packstack.txt
-    sed -i -e 's/CONFIG_NTP_SERVERS=.*/CONFIG_NTP_SERVERS=10.3.252.26/g' ensimag-packstack.txt
-    sed -i -e 's/CONFIG_NEUTRON_ML2_TYPE_DRIVERS=.*/CONFIG_NEUTRON_ML2_TYPE_DRIVERS=vxlan,flat,vlan/g' ensimag-packstack.txt
-    sed -i -e 's/CONFIG_NEUTRON_ML2_FLAT_NETWORKS=.*/CONFIG_NEUTRON_ML2_FLAT_NETWORKS=extnet/g' ensimag-packstack.txt
-    sed -i -e 's/CONFIG_NEUTRON_ML2_VLAN_RANGES=.*/CONFIG_NEUTRON_ML2_VLAN_RANGES=extnet:2232:2232/g' ensimag-packstack.txt
-    sed -i -e 's/CONFIG_NEUTRON_OVS_BRIDGE_IFACES=.*/CONFIG_NEUTRON_OVS_BRIDGE_IFACES=br-ex:eno1/g' ensimag-packstack.txt
-    sed -i -e 's/CONFIG_NEUTRON_OVS_BRIDGEs_COMPUTE=.*/CONFIG_NEUTRON_OVS_BRIDGEs_COMPUTE=br-ex/g' ensimag-packstack.txt
-    sed -i -e 's/CONFIG_PROVISION_DEMO=.*/CONFIG_PROVISION_DEMO=n/g' ensimag-packstack.txt
-
-    #Configure OpenStack
-    source keystonerc_admin
-    openstack user create --domain default --password-prompt neutron
-    openstack role add --project services --user neutron admin
-    openstack service create --name neutron --description "OpenStack Networking" network
-    #Endpoints
-    openstack endpoint create --region RegionOne network public http://controller:9696
-    openstack endpoint create --region RegionOne network internal http://controller:9696
-    openstack endpoint create --region RegionOne network admin http://controller:9696
-
-    #Connect with the external network
-    neutron net-create --router:external --provider:network_type vlan --provider:physical_network extnet --provider:segmentation_id 2232 public
-    neutron subnet-create --name public-subnet --enable_dhcp=False --allocation-pool=start=10.11.54.70,end=10.11.54.89 --gateway=10.11.54.1 public 10.11.54.1/24
-}
-
-openstack_compute()
-{
-}
-
-
-# Use this to start the vpn in a separate console
-# sudo openvpn --config vpnlab2017.conf
-
-
-echo "Connecting to distant host @10.11.51.144"
-ssh -o StrictHostKeyChecking=no root@10.11.51.144 "$(typeset -f); network; openstack_install"
-
-
-echo "Connecting to distant host @10.11.51.145"
-ssh -o StrictHostKeyChecking=no root@10.11.51.145 "$(typeset -f); network; openstack_install"
-
